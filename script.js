0dd2381e93335f1c0b85fb45541812aafba64406
manalkhattak <manalmasood828@gmail.com>
2024-01-25 21:35:28 +0500

    while loop

---
+++ script.js
@@ -0,0 +1,70 @@
+// for (a=1;a<=5;a++){
+//     square = a**2
+//     if (square>20){
+//         break
+    
+//     console.log(square)
+//     }
+// }
+// a = 1
+// while (a<=5){
+//     square = a**2
+//     if (square>20){
+//         break
+//     }
+//     else{
+//     console.log(square)
+// }
+// }
+
+// FOR LOOP
+// dishes = []
+// for (a=0;a<=5;a++){
+//     favDish = prompt("What is your favourite dish? or press q to quit.")
+//     if (favDish!='q'){
+//     dishes.push(favDish)
+//     }
+//     else{
+//         break
+//     }
+// }
+// console.log(dishes)
+
+// WHILE LOOP
+// counter = 0
+// while (counter < 10){
+//     square = counter**2
+//     if (square > 50){
+//         break
+//     }
+//     else{
+//     console.log(square)
+//     counter++
+//     }
+// }
+
+// counter = 10
+// while (counter > 0){
+//     counter -= 1
+//     square = counter**2
+//     if (square > 5){
+//         console.log(square)
+//     }
+//     else{
+//         break
+//     }
+// }
+
+// dishes = []
+// flag = true
+// while (flag){
+//     dishName = prompt("Enter a dish name or press q to quit.")
+//     if (dishName!='q'){
+//     dishes.push(dishName)
+//     }
+//     else{
+//         flag = false
+//     }
+// }
+// console.log(dishes)
+
